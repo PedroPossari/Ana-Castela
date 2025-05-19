e28df685c24f9ab467794979d99a701f3acf6723
Pedro Aparecido Possari <pedropossari2@gmail.com>
2025-05-19 00:18:05 -0300

    Arrumando responsividade mobile PostGrid

--- app/components/organisms/PostGrid.tsx
+++ app/components/organisms/PostGrid.tsx
@@ -9,16 +9,12 @@ export function PostGrid() {
       <SectionTitle>A Trajetória de Sucesso nos Prêmios</SectionTitle>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-8">
         {mockPosts.map((post) => (
-          <Link
-            key={post.id}
-            href={`${post.url}`}
-            className="block group"
-          >
+          <Link key={post.id} href={`${post.url}`} className="block group">
             <div
               className="transition-all duration-300 ease-in-out 
-                group-hover:scale-[1.02] group-hover:shadow-lg 
-                active:scale-[1.02] active:shadow-lg
-                touch-none"
+    group-hover:scale-[1.02] group-hover:shadow-lg 
+    active:scale-[1.02] active:shadow-lg
+    active:opacity-90"
             >
               <BlogPostCard post={post} />
             </div>
