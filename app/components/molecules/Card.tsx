3fa8356d70b84bca0c7b129b468b5d08bf62fee3
Pedro Aparecido Possari <pedropossari2@gmail.com>
2025-05-22 10:09:17 -0300

    arrumando tradução

--- app/components/molecules/Card.tsx
+++ app/components/molecules/Card.tsx
@@ -1,7 +1,7 @@
-// components/molecules/Card.tsx
 import { Text } from "../atoms/Text";
 import { Media } from "../Midia/Midia";
 import { ContentCardProps } from "../../types";
+import { useTranslation } from "react-i18next";
 
 export const Card = ({
   title,
@@ -10,21 +10,25 @@ export const Card = ({
   imageAlt,
   index,
   className = "",
-}: ContentCardProps) => (
-  <div
-    className={`bg-white rounded-xl shadow-md overflow-hidden mb-8 hover:shadow-lg transition-shadow ${className}`}
-  >
-    <div className="md:flex">
-      <div className="md:flex-shrink-0 md:w-1/3">
-        <Media src={imageSrc} alt={imageAlt} />
-      </div>
-      <div className="p-6 md:p-8">
-        <span className="inline-block bg-purple-300 text-gray-800 font-bold px-3 py-1 rounded-full text-sm mb-3">
-          Curiosidade #{index + 1}
-        </span>
-        <Text as="h3">{title}</Text>
-        <Text>{content}</Text>
+}: ContentCardProps) => {
+  const { t } = useTranslation();
+
+  return (
+    <div
+      className={`bg-white rounded-xl shadow-md overflow-hidden mb-8 hover:shadow-lg transition-shadow ${className}`}
+    >
+      <div className="md:flex">
+        <div className="md:flex-shrink-0 md:w-1/3">
+          <Media src={imageSrc} alt={imageAlt} />
+        </div>
+        <div className="p-6 md:p-8">
+          <span className="inline-block bg-purple-300 text-gray-800 font-bold px-3 py-1 rounded-full text-sm mb-3">
+            {t("card.curiosityLabel", { index: index + 1 })}
+          </span>
+          <Text as="h3">{title}</Text>
+          <Text>{content}</Text>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
